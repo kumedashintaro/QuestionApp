fab47672fb0d3a043e36f650d81099975f492895
Kumeda Shintaro <pii.k2.bys@gmail.com>
2020-08-28 11:54:08 +0900

    Initial Commit

---
+++ QuestionApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QuestionApp
+//
+//  Created by 久米田晋太郎 on 2020/08/28.
+//  Copyright © 2020 Shintaro Kumeda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
