d687f46d51fd24b833fa506ee30891bb99d4cb9e
Kumeda Shintaro <pii.k2.bys@gmail.com>
2020-08-30 22:30:19 +0900

    最初のコミット

---
+++ QuestionApp/Model/imagesList.swift
@@ -0,0 +1,30 @@
+//
+//  imagesList.swift
+//  QuestionApp
+//
+//  Created by 久米田晋太郎 on 2020/08/28.
+//  Copyright © 2020 Shintaro Kumeda. All rights reserved.
+//
+
+import Foundation
+
+class ImagesList{
+    
+    var list = [ImagesModel]()
+    
+    init(){
+        
+        list.append(ImagesModel(imageName: "0", correctOrNot: true))
+        list.append(ImagesModel(imageName: "1", correctOrNot: false))
+        list.append(ImagesModel(imageName: "2", correctOrNot: false))
+        list.append(ImagesModel(imageName: "3", correctOrNot: false))
+        list.append(ImagesModel(imageName: "4", correctOrNot: true))
+        list.append(ImagesModel(imageName: "5", correctOrNot: false))
+        list.append(ImagesModel(imageName: "6", correctOrNot: true))
+        list.append(ImagesModel(imageName: "7", correctOrNot: true))
+        list.append(ImagesModel(imageName: "8", correctOrNot: false))
+        list.append(ImagesModel(imageName: "9", correctOrNot: true))
+        list.append(ImagesModel(imageName: "10", correctOrNot: true))
+    }
+    
+}
